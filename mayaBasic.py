734d3edebb4b3ea710c4781730b0da64f2ba87d2
n-designdev <49777717+n-designdev@users.noreply.github.com>
2019-06-12 18:24:18 +0900

    ひとまず動くところまで。

---
+++ mayaBasic.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+
+import maya.cmds as mc
+import pymel.core as pm
+import maya.mel as mel
+import re
+
+import os
+
+
+def newScene ():
+    mc.file(new=True)
+
+def saveAs (outputPath):
+    print 'save start'*5
+    ext = os.path.splitext(outputPath)[1]
+    mc.file(rn=outputPath)
+    if ext == '.ma':
+        mc.file(f=True, s=True, type='mayaAscii')
+    else:
+        mc.file(f=True, s=True, type='mayaBinary')
+    print 'save end'*5
+
+def save ():
+    print 'save!!!'*10
+    mc.file(s=True, f=True)
+
+def replaceAsset (assetPath, namespace):
+    mc.warning( 'replace start ')
+    refs = mc.ls(type='reference')
+    try:
+        print refs
+        refs.remove('sharedReferenceNode')
+    except:
+        pass
+    tgtRN = ''
+    for r in refs:
+        ns = mel.eval('referenceQuery -ns ' + r)[1:]
+        if namespace == ns:
+            tgtRN = r
+            break
+    else:
+        mc.error('can not replace')
+
+    # print assetPath, tgtRN
+    try:
+        mc.file(assetPath, loadReference=tgtRN)
+    except:
+        pass
+    mc.warning('replace end')
+
+def camreplaceAsset (assetPath, namespace):
+    mc.warning( 'replace start ')
+    # refs = mc.ls(type='reference')
+    # try:
+    #     print refs
+    #     # refs.remove('sharedReferenceNode')
+    # except:
+    #     pass
+    # tgtRN = ''
+    # for r in refs:
+    #     ns = mel.eval('referenceQuery -ns ' + r)[1:]
+    #     if namespace == ns:
+    #         tgtRN = r
+    #         break
+    # else:
+    #     mc.error('can not replace')
+
+    # # print assetPath, tgtRN
+    # try:
+    #     mc.file(assetPath, loadReference=tgtRN)
+    # except:
+    #     pass
+    mc.warning('replace end')
+
+
+def exportFile (outputPath, topNode):
+    mc.warning('export start')
+    mc.select(topNode)
+    mc.file(outputPath, typ='mayaAscii', f=True, es=True, pr=True)
+    mc.warning('export end')
+
+def loadAsset (assetPath , namespace):
+    print 'loadAsset start'*5
+    mc.file(assetPath, r=True, namespace=namespace, mergeNamespacesOnClash=False, ignoreVersion=True)
+    print 'loadAsset end'*5
+
+def importAsset (animPath, namespace):
+    with open(os.path.dirname(os.path.dirname(animPath))+'/cal_grb.txt') as f:
+        a = f.readline().strip()
+        b = f.readline().strip()
+        camtype = f.readline().strip()
+    print camtype
+    print os.path.dirname(os.path.dirname(animPath))
+
+    if camtype == 'aim':
+        print 'camtype is "aim_camera"'
+        camPath_neo = 'E:/users/ueda/camTestaim.ma'
+        # camPath_neo = r'P:\proj\TMST\elm\_cam\cam\rig\3D\maya\master\cam.cur.ma'
+    elif camtype == '2D':
+        print 'camtype is "2D_camera"'
+        camPath_neo = 'E:/users/ueda/camTest2D.ma'
+        # camPath_neo = r'P:\proj\TMST\elm\_cam\cam\rig\2D\maya\master\cam.cur.ma'
+    elif camtype == '3D':
+        print 'camtype is "3D_camera"'
+        camPath_neo = 'E:/users/ueda/camTest.ma'
+        # camPath_neo = r'P:\proj\TMST\elm\_cam\cam\rig\Aim\maya\master\camAim.cur.ma'
+    else:
+        print 'camtype cant unload....'
+
+    mc.file(camPath_neo,i=True, ignoreVersion=True, preserveReferences=True,mergeNamespacesOnClash=False, importFrameRate=True, importTimeRange='override')
+
+    # ---outputPath----
+    # E:/Project/b/shots/d/e/NBB062/publish/test_charSet/cameraA/v062/anim/BG.ma
+    # ----camPath----
+    # P:/proj/TMST/elm/_cam/cam/rig/3D/maya/master/cam.cur.ma
+    # print 'import Asset end'
+
+def renameAsset(namespace, animPath):
+    mc.select('cloCamera_grp',hi=True)
+    x=pm.ls(sl=True,)
+    for i in x:
+        i.rename(re.sub('^',namespace+'_',i.name()))
+
+    print os.path.dirname(os.path.dirname(animPath))+'/cal_grb.txt'
+
+    with open(os.path.dirname(os.path.dirname(animPath))+'/cal_grb.txt') as f:
+        a = f.readline().strip()
+        b = f.readline().strip()
+
+    x = pm.ls('NAX000')
+    x[0].rename(a)
+    y = pm.ls('frame_1_120')
+    y[0].rename(b)
+
+def attachABC (abcPath, hierarchyList):
+    if not mc.pluginInfo('AbcImport', q=True, l=True):
+        mc.loadPlugin('AbcImport')
+    hierarchy = ' '.join(hierarchyList)
+    mel.eval('AbcImport -mode import -fitTimeRange -debug -connect ' + '\"' + hierarchy + '\" ' + '\"' + abcPath + '\"')
+
+def replaceABCPath (repAbcPath):
+    abcNodes = mc.ls(type='AlembicNode')
+    if len(abcNodes) != 0:
+        mc.setAttr(abcNodes[0]+'.abc_File', repAbcPath, type='string')
+    print 'x' * 20
+
+def delUnknownNode ():
+    unknownNodes = mc.ls(type='unknown')
+    if len(unknownNodes) != 0:
+        mc.delete(unknownNodes)
+
+def setEnv ():
+    os.environ['VRAY_FOR_MAYA2015_MAIN_X64'] = 'Y:\\users\\env\\vray\\maya2015_vray_adv_36004\\maya_vray'
+    os.environ['VRAY_TOOLS_MAYA2015_X64'] = 'Y:\\users\\env\\vray\\maya2015_vray_adv_36004\\vray\\bin'
+    os.environ['MAYA_PLUG_IN_PATH'] = 'Y:/users/env/vray/maya2015_vray_adv_36004/maya_vray/plug-ins;'+os.environ['MAYA_PLUG_IN_PATH']
+    os.environ['VRAYPATH'] = 'Y:\\users\\env\\vray\\maya2015_vray_adv_36004'
+    os.environ['VRAY_FOR_MAYA2015_PLUGINS_X64'] = 'Y:/users/env/vray/maya2015_vray_adv_36004/maya_vray/vrayplugins'
+    os.environ['MAYA_SCRIPT_PATH'] = 'Y:/users/env/vray/maya2015_vray_adv_36004/maya_vray/scripts;'+os.environ['MAYA_SCRIPT_PATH']
+    os.environ['VRAY_VER'] = '36004'
+    os.environ['VRAY_AUTH_CLIENT_FILE_PATH'] = 'Y:\\users\\env\\maya\\lic'
+    os.environ['VRAY_OSL_PATH_MAYA2015_X64'] = 'Y:\\users\\env\\vray\\maya2015_vray_adv_36004\\vray\\opensl'
+
+    for k,v in os.environ.items():
+        print k, v
+
+
+# def bakeKeys (topNode):
+#     sframe = mc.playbackOptions(q=True, min=True)
+#     eframe = mc.playbackOptions(q=True, max=True
+#     mc.bakeResults(topNode, simulation=True, t=(sframe, eframe), hierarchy='below', sampleBy=1, dic=True, pok=True, sac=False, ral=False, bol=False, mr=True, cp=False, shape=True)
+
+#     constraints = []
+#     constraints += mc.ls(topNode, dag=True, type='parentConstraint')
+#     constraints += mc.ls(topNode, dag=True, type='pointConstraint')
+#     constraints += mc.ls(topNode, dag=True, type='orientConstraint')
+#     constraints += mc.ls(topNode, dag=True, type='scaleConstraint')
+#     for constraint in constraints:
+#         if mc.referenceQuery(constraint, inr=True): continue
+#         mc.delete(constraint)
+
+
+if __name__ == '__main__':
+    namespace = 'DDNinaNml'
+    assetPath = 'P:/Project/mem2/assets/chara/DDNina/DDNinaNml/publish/model/RenderHigh/maya/current/DDNinaNml_mdlRH.mb'
+    topNode = 'DDNinaNml'
+    abcPath = 'P:/Project/mem2/shots/roll04/s116D/c009D/publish/animGeo/s116Dc009D_anm_v004.ma/s116Dc009D_animGeoCache_DDNinaNml.abc'
+
+    loadAsset(assetPath, namespace)
+
+    selHierarchy = mc.ls(namespace+':'+topNode, dag=True)
+
+    attachABC(abcPath, selHierarchy)
---
+++ util.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import shutil
+import distutils.dir_util
+
+# publishpath = shotpath + 'publish' + 'charSet' + char
+# publishfullpath = publishpath + currentVer
+# publishfullabcpath = publishfullpath + 'abc'
+
+class outputPathConf (object):
+
+    def __init__ (self, inputPath, isAnim=False, test=True):
+        self.inputPath = inputPath.replace('\\', '/')
+        self.isAnim = True
+        self.isAbc = False
+        self.outputRootDir = 'charSet'
+        self.outputCamRootDir = 'Cam'
+        if test:
+            self.outputRootDir = 'test_charSet'
+            self.outputCamRootDir = 'test_Cam'
+        print self.inputPath
+        match = re.match('(E:/Project/[a-zA-Z0-9]+)/([a-zA-Z0-9]+)/([a-zA-Z0-9]+)/([a-zA-Z0-9]+)/([a-zA-Z0-9]+)', self.inputPath)
+        if match is None:
+            raise ValueError('directory structure is not n-design format')
+
+        self._project  = match.group(1)
+        self._roll     = match.group(3)
+        self._sequence = match.group(4)
+        self._shot     = match.group(5)
+        self._shotpath = os.path.join(self._project, 'shots', self._roll, self._sequence, self._shot)
+
+    def createOutputDir (self, char):
+        self._publishpath = os.path.join(self._shotpath, 'publish', self.outputRootDir, char)
+        if os.path.exists(self._publishpath):
+            self.verInc()
+        else:
+            try:
+                os.makedirs(self._publishpath)
+                self.verInc()
+            except:
+                pass
+
+    def createCamOutputDir (self, cameraName):
+        self._publishpath = os.path.join(self._shotpath, 'publish', self.outputCamRootDir, cameraName)
+        if os.path.exists(self._publishpath):
+            self.verInc()
+        else:
+            try:
+                os.makedirs(self._publishpath)
+                self.verInc()
+            except:
+                pass
+
+    def verInc (self):
+        vers = os.listdir(self._publishpath)
+        if len(vers) == 0:
+            self._currentVer = 'v001'
+        else:
+            vers.sort()
+            currentVer = vers[-1]
+            currentVerNum = int(currentVer[1:])
+            nextVerNum = currentVerNum + 1
+            nextVer = 'v' + str(nextVerNum).zfill(3)
+            self._currentVer = nextVer
+        self._publishfullpath = os.path.join(self._publishpath, self._currentVer)
+        self._publishfullabcpath = os.path.join(self._publishfullpath, 'abc')
+        self._publishfullanimpath = os.path.join(self._publishfullpath, 'anim')
+        self._publishfullcampath = os.path.join(self._publishfullpath, 'anim')
+        try:
+            os.mkdir(self._publishfullpath)
+            if self.isAnim:
+                os.mkdir(self._publishfullanimpath)
+            elif self.isAbc:
+                os.mkdir(self._publishfullabcpath)
+            else:
+                os.mkdir(self._publishfullcampath)
+        except:
+            pass
+
+    def makeCurrentDir (self):
+        currentDir = os.path.join(self.publishpath, 'current')
+        self._publishcurrentpath = currentDir
+        # if os.path.exists(currentDir):
+        #     shutil.rmtree(currentDir)
+        # shutil.copytree(self._publishfullpath, currentDir)
+        distutils.dir_util.copy_tree(self._publishfullpath, currentDir)
+
+    def removeDir (self):
+        if os.path.exists(self._publishpath+'/current'):
+            files = os.listdir(self._publishpath+'/current')
+            for f in files:
+                if '.ma' in f:
+                    return
+        shutil.rmtree(self._publishpath)
+
+    @property
+    def sequence (self):
+        return self._sequence
+
+    @property
+    def shot (self):
+        return self._shot
+
+    @property
+    def publishpath (self):
+        return self._publishpath.replace(os.path.sep, '/')
+
+    @property
+    def publishfullpath (self):
+        return self._publishfullpath.replace(os.path.sep, '/')
+
+    @property
+    def publishfullabcpath (self):
+        return self._publishfullabcpath.replace(os.path.sep, '/')
+
+    @property
+    def publishfullanimpath (self):
+        return self._publishfullanimpath.replace(os.path.sep, '/')
+
+    @property
+    def publishfullcampath (self):
+        return self._publishfullcampath.replace(os.path.sep, '/')
+
+    @property
+    def publishcurrentpath (self):
+        return self._publishcurrentpath.replace(os.path.sep, '/')
